a1a7bdefa7ee8596de3236507b475f1e29e36bf6
negasora <negasora@negasora.com>
2022-08-26 23:34:22 -0400

    Dicegang old

---
+++ dice/wasm/a.py
@@ -0,0 +1,27 @@
+first = "dice{"
+second = "F!3lD"
+
+#third = bytes([100, 48, 78, 117, 55])
+third = "d0Nu7"
+
+fourth = "cwrap"
+
+#fifth = bytes([109, 64, 120, 33, 77])
+fifth = "m@x!M"
+
+
+
+for c in range(20, 200):
+    for d in range(20, 200):
+        if (d+1546) * (c + 3913) == 6431119:
+            print(c,d)
+#e == 125
+#(d+1546) * (c + 3913) == 6431119
+
+sixth = bytes([84, 36, 114, 51, 125])
+print(sixth)
+sixth = "T$r3}"
+
+
+
+print(first+second+third+fourth+fifth+sixth)
